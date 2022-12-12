aaf11d0c8cbe27ed2a64b815dbd4ef28ae9196be
Dejan Milicic <dejan.milicic@gmail.com>
2022-12-13 00:29:59 +0100

    lead model

---
+++ LeadsApi/LeadsApi/Lead.cs
@@ -0,0 +1,27 @@
+﻿namespace LeadsApi
+{
+    public class Lead
+    {
+        public string Id { get; set; }
+
+        public string User { get; set; }
+
+        public int Day { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Company { get; set; }
+
+        public string JobTitle { get; set; }
+
+        public string Country { get; set; }
+
+        public string Linkedin { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
